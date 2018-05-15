0954a7dda78a82290e47ab0bb924caf10bbeddc4
danmerron-sig <dmerron@sportingsolutions.com>
2018-05-15 14:06:53 +0100

    added option for tldn

--- dns.tf
+++ dns.tf
@@ -1,7 +1,35 @@
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "subdomain-www" {
   zone_id = "${var.zoneid}"
   name    = "www.${var.name}"
   type    = "A"
+  count = "${var.tldn}"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "subdomain-root" {
+  zone_id = "${var.zoneid}"
+  name    = "${var.name}"
+  type    = "A"
+  count = "${var.tldn}"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+
+resource "aws_route53_record" "www" {
+  zone_id = "${var.zoneid}"
+  name    = "www"
+  type    = "A"
+  count = "${var.tldn}"
 
   alias {
     name                   = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
@@ -12,8 +40,9 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_record" "root" {
   zone_id = "${var.zoneid}"
-  name    = "${var.name}"
   type    = "A"
+  count = "${var.tldn}"
+  name = ""
 
   alias {
     name                   = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
