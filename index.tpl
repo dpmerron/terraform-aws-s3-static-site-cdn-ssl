dbae0df8976ee880cbcd38869061a42282454ebb
danmerron-sig <dmerron@sportingsolutions.com>
2018-05-15 15:26:37 +0100

    fix

---
+++ index.tpl
@@ -0,0 +1,3 @@
+${domain} </br>
+
+Static website hosted on S3 deployed using Terraform Module created by Dan Merron: }https://github.com/dpmerron/terraform-aws-s3-static-site-cdn-ssl
--- templates.tf
+++ templates.tf
@@ -1,15 +1,7 @@
 data "template_file" "init" {
-  template = "${file("policy.tpl")}"
+  template = "${file("${path.module}/policy.tpl")}"
 
   vars {
     policy_name = "${var.name}.${var.domain}"
-  }
-}
-
-data "template_file" "wwwinit" {
-  template = "${file("www-policy.tpl")}"
-
-  vars {
-    policy_name = "www.${var.name}.${var.domain}"
   }
 }
